c78c934f0606fbd769e29d9857187fad24545d0e
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-10-06 15:21:38 +1200

    tests and referee

--- verification/tests.py
+++ verification/tests.py
@@ -10,27 +10,40 @@ Each test is dict with
 
 TESTS = {
     "Basics": [
-        {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
-        },
-        {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
+        {"input": [3, 2, 1, 4], "answer": 20},
+        {"input": [1, 1, 1, 1], "answer": 4},
+        {"input": [4, 4, 4, 1], "answer": 4},
+        {"input": [18, 17, 15, 83], "answer": 5472},
+        {"input": [10, 38, 4, 80], "answer": 2145},
+        {"input": [3, 1, 32, 39], "answer": 264},
+        {"input": [24, 49, 38, 40], "answer": 11521},
+        {"input": [5, 46, 40, 78], "answer": 11191},
+        {"input": [15, 9, 15, 33], "answer": 2504},
+        {"input": [5, 24, 49, 46], "answer": 4875},
+        {"input": [17, 29, 50, 82], "answer": 26980},
+        {"input": [17, 3, 48, 48], "answer": 2808},
+        {"input": [29, 21, 8, 56], "answer": 5936}
     ],
     "Extra": [
-        {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
-        },
-        {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
+        {"input": [23, 42, 18, 96], "answer": 19608},
+        {"input": [3, 39, 49, 91], "answer": 8000},
+        {"input": [11, 2, 39, 78], "answer": 1440},
+        {"input": [50, 13, 15, 40], "answer": 6048},
+        {"input": [20, 16, 39, 64], "answer": 13994},
+        {"input": [14, 8, 23, 60], "answer": 3240},
+        {"input": [9, 44, 16, 66], "answer": 7640},
+        {"input": [18, 1, 27, 82], "answer": 1064},
+        {"input": [39, 33, 14, 81], "answer": 20365},
+        {"input": [31, 3, 18, 33], "answer": 1782},
+        {"input": [33, 35, 29, 43], "answer": 14280},
+        {"input": [2, 44, 3, 42], "answer": 486},
+        {"input": [35, 21, 50, 61], "answer": 26642},
+        {"input": [17, 23, 27, 67], "answer": 12096},
+        {"input": [47, 35, 19, 46], "answer": 14484},
+        {"input": [30, 40, 25, 61], "answer": 26036},
+        {"input": [1, 25, 7, 44], "answer": 416},
+        {"input": [50, 50, 11, 100], "answer": 30926},
+        {"input": [35, 11, 45, 37], "answer": 6600},
+        {"input": [48, 1, 41, 45], "answer": 2100}
     ]
 }
